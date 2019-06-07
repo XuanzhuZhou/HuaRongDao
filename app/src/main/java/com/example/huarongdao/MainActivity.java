948890694af540a55fc3dab968fc02df3efd3b72
yangmingkai <yangmingkai@shuidihuzhu.com>
2019-06-07 12:10:15 +0800

    @Title:设置开始按钮的动画

--- app/src/main/java/com/example/huarongdao/MainActivity.java
+++ app/src/main/java/com/example/huarongdao/MainActivity.java
@@ -1,34 +1,63 @@
 package com.example.huarongdao;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    public static boolean levels[] = {true,true,true,true};
+    public static boolean levels[] = {true, true, true, true};
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        final TextView startGame = findViewById(R.id.start_game);
+        final Button levelBtn = findViewById(R.id.level_btn);
+        TextView aboutGame = findViewById(R.id.about);
+        startGame.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        levelBtn.setBackgroundResource(R.drawable.surprise_icon);
+                        startGame.setTextColor(getResources().getColor(R.color.colorGrey));
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        levelBtn.setBackgroundResource(R.drawable.back_icon);
+                        startGame.setTextColor(getResources().getColor(R.color.colora));
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
 
     public void chooselevel(View view) {
         Intent intent = new Intent();
-        intent.setClass(MainActivity.this,LevelActivity.class);
+        intent.setClass(MainActivity.this, LevelActivity.class);
         startActivity(intent);
     }
 
     public void chooseabout(View view) {
         Intent intent = new Intent();
-        intent.setClass(MainActivity.this,AboutActivity.class);
+        intent.setClass(MainActivity.this, AboutActivity.class);
         startActivity(intent);
     }
     /**
-    level page 跳转
-    */
+     level page 跳转
+     */
 
 }
